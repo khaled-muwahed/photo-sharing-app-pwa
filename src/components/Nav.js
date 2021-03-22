00fab5ca07d09d3247d76fea4cbe7afcd103ddb8
khaled-muwahed <52414740+khaled-muwahed@users.noreply.github.com>
2021-03-22 00:25:32 +0000

    Implementing Router nav

---
+++ src/components/Nav.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useRouteMatch,
+    useParams
+  } from "react-router-dom";
+
+
+export default function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/login">Login</Link>
+                </li>
+                <li>
+                    <Link to="/signup">Sign Up</Link>
+                </li>
+            </ul>
+
+        </nav>
+    )
+}
