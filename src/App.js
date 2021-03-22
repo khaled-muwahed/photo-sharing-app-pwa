9c17a8124a25096c38c221f8961e1c8fc040223a
khaled-muwahed <52414740+khaled-muwahed@users.noreply.github.com>
2021-03-22 00:25:09 +0000

    Router Nav

--- src/App.js
+++ src/App.js
@@ -5,61 +5,84 @@ import { CssBaseline, Grid } from '@material-ui/core';
 import Login from "./containers/Login";
 import TopNav from './components/TopNav';
 import BottomNav from './components/BottomNav';
+import SignIn from './containers/SignIn';
+import TestLoginForm from './containers/LoginTest';
+import SignUp from './containers/SignUp';
+import GetData from './containers/Home';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import Nav from './components/Nav'
+import { Home } from '@material-ui/icons';
 
 //
-const container =  {
+const container = {
   flex: 1,
   flexDirection: "column"
 };
 
 const divStyle = {
-  //flex : 1,
-  
-  //flexDirection: 'row',
- // alignItems: "flex-end",
- //justifyContent: "flex-end",
-  //alignSelf: "center"
   position: 'absolute',
-  bottom:0,
-  width:"100%"
+  bottom: 0,
+  width: "100%"
 
-    
-
-    
-
+};
+const styles = {
+  //height: '100vh',
+  overflow: "auto",
+  textAlign: "center",
+  marginTop: 50
+};
+const BottomBarStyle = {
+  position: 'fixed',
+  bottom: 0,
+  width: "100%",
+  marginTop: 20
 
 };
 
 
+// <Login/> fully working ***
 function App() {
- // const [tab, setTab] = useState(0);
- //  <BottomNav value = {tab} onChange = {setTab}/>
+  // const [tab, setTab] = useState(0);
+  //  <BottomNav value = {tab} onChange = {setTab}/>
   return (
-    <div style = {container}>
-      <Grid container direction="column">
-      <TopNav/>
-        <div style={styles}>
+    <Router>
+      <div style={{ position: "fixed",
+            top: 0,
+            width: "100%",
+            }}>
+      <TopNav  />
+      </div>
+      <div style={container}>
+        <Grid container direction="column">
+          
+          <div style={styles}>
+            <Nav  />
+           
+            <Switch>
+            <Route path="/signup" component = {SignUp} />
+            <Route path="/login" component = {Login} />
+            <Route path="/" exact component = {GetData} />
+            </Switch>
+            
+
+
+          </div>
+
+          <div style={BottomBarStyle}>
+          <BottomNav />
+
+          </div>
+        </Grid>
         
-          <Login />
-         
-        </div>
+        <CssBaseline />
         
-        <div style = {divStyle}>
-        <BottomNav />
-      
-        </div>
-      </Grid>
-      <CssBaseline />
-    </div>
+      </div>
+    </Router>
   );
 }
 
 export default App;
 
 
-const styles = {
- 
-  //height: '100vh',
-  overflow: "auto",
-  textAlign: "center"
-};
