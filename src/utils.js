9e7dc366515d0a5b9b0955bd34bde59b2aac0a68
khaled-muwahed <52414740+khaled-muwahed@users.noreply.github.com>
2021-04-18 14:04:51 +0100

    adding a component to control urls

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,7 @@
+//TO RUN ON YOUR MACHINE REPLACE THE LINK WITH http://localhost:3333/
+
+//TO TEST ON YOU LOCAL NETWORK (PHONE ETC ... ) REPLACE THE LINK WITH YOUR IP
+
 export const getApiUrl = url => {
     const host = 'http://192.168.1.104:3333';
     return `${host}${url}`;
